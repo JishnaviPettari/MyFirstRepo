9959aebd8ecc63975c23d7766cf3390a530c52f8
jishnavi <jishu@gmail.com>
2025-09-06 10:37:50 +0000

    sum sub

---
+++ app.java
@@ -0,0 +1,12 @@
+// Simple Addition Program in Java
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 10;   // first number
+        int num2 = 20;   // second number
+
+        int sum = num1 + num2;  // perform addition
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
+
